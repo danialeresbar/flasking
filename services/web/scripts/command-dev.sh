a1cb8c72a9b0e246e14616bcfedfbbff266fc38f
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-26 23:25:36 -0500

    Update entrypoint

--- services/web/scripts/command-dev.sh
+++ services/web/scripts/command-dev.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 python web/manage.py create_database
+
+exec "$@"
+
 python web/manage.py run -h 0.0.0.0
