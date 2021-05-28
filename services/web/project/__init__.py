b180b283ae8c28461c4deb4f31e461df539a57a8
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-28 15:49:17 -0500

    Add static files serve settings

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -21,3 +21,7 @@ class User(database.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
